4ca9f20a637fe38ccabb86f3a44a292967b26bd4
Harshit Anand <harshitanand97@gmail.com>
2021-09-02 10:43:25 +0530

    feat: new functionality of next button

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const billAmount = document.getElementById("bill-amount");
 const cashGiven = document.getElementById("cash-given");
 const checkBtn = document.getElementById("check");
 const notes = document.querySelectorAll(".noOfNotes");
+const nextBtn = document.getElementById("next");
+const cashLabel = document.getElementById("cash");
+
+cashLabel.style.display = "none";
+checkBtn.style.display="none";
+cashGiven.style.display="none";
+
+nextBtn.addEventListener("click",()=>{
+    if(billAmount.value <0){
+        alert("Bill amount can't be less than 0");
+    }
+    else{
+        cashLabel.style.display = "block";
+        checkBtn.style.display="block";
+        cashGiven.style.display="block";
+    }
+})
+
 
 const denominations = [2000,500,100,20,10,5,1];
 
